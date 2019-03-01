97c236c5ef3f0d68ad355f9b5b09e803fb56831a
Ari Chivukula <ari@chivuku.la>
2019-03-01 13:29:47 -0800

    Create index.ts

---
+++ test/index.ts
@@ -0,0 +1,17 @@
+import "mocha";
+
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+import {
+  noop,
+} from "../source/index";
+
+chai.use(chaiAsPromised);
+
+it(
+  "noop",
+  () => {
+    noop();
+  },
+);
