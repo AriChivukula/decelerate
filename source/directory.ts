0c9d0418deca8b4086d17aa93a57fb40ec784a7a
Ari Chivukula <ari@chivuku.la>
2019-03-31 06:53:00 +0300

    Create directory.ts

---
+++ source/directory.ts
@@ -0,0 +1,32 @@
+import {
+  WorkbookParser,
+} from "./workbook";
+
+export type DirectoryParser = async (directory: IDirectory) => Promise<void>;
+
+export interface IDirectory {
+  bindToSubDirectory(name: string, parser: DirectoryParser): this;
+  bindToSubDirectories(match: RegExp, parser: DirectoryParser): this;
+  bindToWorkbook(name: string, parser: WorkbookParser): this;
+  bindToWorkbooks(match: RegExp, parser: WorkbookParser): this;
+}
+
+export class Directory implements IDirectory {
+
+  bindToSubDirectory(name: string, parser: DirectoryParser): this {
+    return this;
+  }
+
+  bindToSubDirectories(match: RegExp, parser: DirectoryParser): this {
+    return this;
+  }
+
+  bindToWorkbook(name: string, parser: WorkbookParser): this {
+    return this;
+  }
+
+  bindToWorkbooks(match: RegExp, parser: WorkbookParser): this {
+    return this;
+  }
+
+}
