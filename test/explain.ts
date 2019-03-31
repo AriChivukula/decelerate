90d544da73885ad1a261bceda769b1f1f65bbec9
Ari Chivukula <ari@chivuku.la>
2019-03-31 07:20:58 +0300

    Update and rename index.ts to explain.ts

---
+++ test/explain.ts
@@ -0,0 +1,73 @@
+import "mocha";
+
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+import {
+  Directory,
+} from "../source/directory";
+import {
+  Workbook,
+} from "../source/workbook";
+import {
+  Sheet,
+} from "../source/sheet";
+import {
+  Column,
+} from "../source/column";
+import {
+  Row,
+} from "../source/row";
+import {
+  Cell,
+} from "../source/cell";
+
+chai.use(chaiAsPromised);
+
+it(
+  "directory",
+  () => {
+    const directory = new Directory();
+    expect(directory.explain()).to.deep.equal({});
+  },
+);
+
+it(
+  "workbook",
+  () => {
+    const workbook = new Workbook();
+    expect(workbook.explain()).to.deep.equal({});
+  },
+);
+
+it(
+  "sheet",
+  () => {
+    const sheet = new Sheet();
+    expect(sheet.explain()).to.deep.equal({});
+  },
+);
+
+it(
+  "column",
+  () => {
+    const column = new Column();
+    expect(column.explain()).to.deep.equal({});
+  },
+);
+
+it(
+  "row",
+  () => {
+    const row = new Row();
+    expect(row.explain()).to.deep.equal({});
+  },
+);
+
+it(
+  "cell",
+  () => {
+    const cell = new Cell();
+    expect(cell.explain()).to.deep.equal({});
+  },
+);
