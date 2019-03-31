7e69a2a65c04a558ac88288467230e3971f69c76
Ari Chivukula <ari@chivuku.la>
2019-03-31 08:02:47 +0300

    Update interfaces.ts

--- source/interfaces.ts
+++ source/interfaces.ts
@@ -1,7 +1,18 @@
+export type TExplained = {
+  parser: string;
+  inner?: {
+    [k: string]: TExplained;
+  };
+};
+
 export interface CanBeExplained {
-  explain(): Object;
+  explain(): TExplained;
 }
 
-export interface CanBeExported<T> {
-  export(): T;
+export type TExport = {
+  [k: string]: Export | boolean | number | string;
+};
+
+export interface CanBeExported {
+  export(): TExport;
 }
