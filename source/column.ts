0f28da38f0e826fd55b99e717dcae21a713ad578
Ari Chivukula <ari@chivuku.la>
2019-03-31 09:48:40 +0300

    Update column.ts

--- source/column.ts
+++ source/column.ts
@@ -7,6 +7,9 @@ import {
   List,
   ListParser,
 } from "./list";
+import {
+  Cell
+} from "./Cell";
 
 export type ColumnParser = ListParser<IColumn>;
 
@@ -15,6 +18,12 @@ export interface IColumn extends IList {
 
 export class Column extends List implements IColumn, CanBeExported {
   export(): TExported {
-    return {};
+    const targets = this.getTargets();
+    const finalTargets: TExported = {};
+    for (const key in targets) {
+      const cell = new Cell();
+      finalTargets[key] = targets[key].parser(cell);
+    }
+    return finalTargets;
   }
 }
