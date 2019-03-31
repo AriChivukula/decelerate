6aa76e9794fb5b5a1b0b0510b8e25978b44fbaff
Ari Chivukula <ari@chivuku.la>
2019-03-31 07:07:35 +0300

    Create column.ts

---
+++ source/column.ts
@@ -0,0 +1,13 @@
+import {
+  IList,
+  List,
+  ListParser,
+} from "./list";
+
+export type ColumnParser = ListParser<IColumn>;
+
+export interface IColumn extends IList {
+}
+
+export class Column extends List implements IColumn {
+}
