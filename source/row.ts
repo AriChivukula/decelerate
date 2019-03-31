1bdd1d2c63c9699d7e1e27b8ed2fa1bf8881b1ee
Ari Chivukula <ari@chivuku.la>
2019-03-31 10:20:40 +0300

    Update row.ts

--- source/row.ts
+++ source/row.ts
@@ -1,4 +1,6 @@
 import {
+  CanBeExplained,
+  TExplained,
   CanBeExported,
   TExported,
 } from "./common";
@@ -7,14 +9,27 @@ import {
   List,
   ListParser,
 } from "./list";
+import {
+  Cell,
+} from "./cell";
 
 export type RowParser = ListParser<IRow>;
 
 export interface IRow extends IList {
+}
+
+export interface IRowTarget extends IListTarget {
   kind: "Row",
 }
 
-export class Row extends List implements IRow, CanBeExported {
+export class Row extends List<IRowTarget> implements IRow, CanBeExplained, CanBeExported {
+  explain(): TExplained {
+    return {
+      parser: this.constructor.name,
+      inner: {},
+    };
+  }
+
   export(): Promise<TExported> {
     const targets = this.getTargets();
     const finalTargets: TExported = {};
