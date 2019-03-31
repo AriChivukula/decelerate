80299bf120f1bc4e748d69dc3602a47c54aafcf0
Ari Chivukula <ari@chivuku.la>
2019-03-31 13:34:02 +0300

    Create list.ts

---
+++ source/list.ts
@@ -0,0 +1,33 @@
+import {
+  ITarget,
+  HasTargets,
+} from "./common";
+import {
+  CellParser,
+} from "./cell";
+
+export type ListParser<T extends IList> = (list: T) => Promise<void>;
+
+export interface IList {
+  bindToCell(name: string, index: number, parser: CellParser): this;
+  bindToCellRange(name: string, start: number, length: number, parser: CellParser): this;
+}
+
+export interface IListTarget extends ITarget {
+  readonly index: number,
+  readonly parser: CellParser,
+}
+
+export abstract class List extends HasTargets<IListTarget> implements IList {
+  bindToCell(name: string, index: number, parser: CellParser): this {
+    return this;
+  }
+
+  bindToCellRange(name: string, start: number, length: number, parser: CellParser): this {
+    return this;
+  }
+
+  getTargetKey(target: IListTarget): string {
+    return target.name + target.index;
+  }
+}
