59da806352fe37adb54c16963507cd942616166d
Ari Chivukula <ari@chivuku.la>
2019-03-31 07:05:12 +0300

    Create list.ts

---
+++ source/list.ts
@@ -0,0 +1,20 @@
+import {
+  CellParser,
+} from "./cell";
+
+export type ListParser<T extends IList> = async (list: T) => Promise<void>;
+
+export interface IList {
+  bindToCell(index: number, parser: CellParser): this;
+  bindToCellRange(start: number, length: number, parser: CellParser): this;
+}
+
+export abstract class List implements IList {
+  bindToCell(index: number, parser: CellParser): this {
+    return this;
+  }
+
+  bindToCellRange(start: number, length: number, parser: CellParser): this {
+    return this;
+  }
+}
