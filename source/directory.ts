2cb7cc93512a1b92d2dfcaed71fba2d6d71fe2b6
Ari Chivukula <ari@chivuku.la>
2019-04-02 08:17:18 -0700

    Update directory.ts

--- source/directory.ts
+++ source/directory.ts
@@ -148,6 +148,7 @@ export class Directory extends HasTargets<IDirectoryDirectoryTarget | IDirectory
   }
   
   private async getMatching(nameMatch: string | RegExp, isFile: boolean): Promise<string[]> {
+    // ts-ignore
     let paths = await promises.readdir(this.path, {withFileTypes: true});
     let matches: string[] = [];
     for (let dirent of paths) {
