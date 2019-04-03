8a5825ade94fde82c1652671a4321b73fe6357d9
Ari Chivukula <ari@chivuku.la>
2019-04-03 08:18:42 -0700

    Create e2e.sh

---
+++ test/e2e.sh
@@ -0,0 +1,18 @@
+set -e
+
+export TEST_INS="$(ls test/e2e | grep '.ts')"
+for TEST_IN in $TEST_INS
+do
+  echo "RUNNING ${TEST_IN}"
+  export TEST_OUT="${TEST_IN::-3}.out"
+  export EXPECTED_OUT="$(cat test/e2e/${TEST_OUT})"
+  export ACTUAL_OUT="$(node build/cli.js --directory test/data --parse ${TEST_IN})"
+  export DIFF="$(diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT}))"
+  if [ -n "$DIFF" ]; then
+    echo "FAILURE ${TEST_OUT}"
+    echo $DIFF
+    exit 1
+  else
+    echo "PASS ${TEST_OUT}"
+  fi
+done
