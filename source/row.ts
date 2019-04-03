21bcf8df5f6e0cd10e5eb3f645a4ae056a186d60
Ari Chivukula <ari@chivuku.la>
2019-04-03 07:14:23 -0700

    Update row.ts

--- source/row.ts
+++ source/row.ts
@@ -1,4 +1,8 @@
 import {
+  WorkSheet,
+} from "xlsx";
+
+import {
   ICanExportAndExplain,
 } from "./common";
 import {
@@ -16,11 +20,18 @@ export interface IRow extends IList {
 }
 
 export class Row extends List implements IRow {
+  constructor(
+    private readonly ws: WorkSheet,
+    private readonly rowIdx: number,
+  ) {
+    super();
+  }
+
   protected async explore(
     appendToOutput: (key: string, value: ICanExportAndExplain) => Promise<void>,
   ): Promise<void> {
     for (const target of this.getTargets()) {
-      const cell = new Cell(target.parser);
+      const cell = new Cell(this.ws, this.rowIdx, target.index, target.parser);
       await appendToOutput(target.name + ":" + target.index, cell);
     }
   }
