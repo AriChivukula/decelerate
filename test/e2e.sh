2748e223ae6e830419fa0c318ccf50a503c8e3b0
Ari Chivukula <ari@chivuku.la>
2019-04-03 08:20:26 -0700

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -4,7 +4,7 @@ export TEST_INS="$(ls test/e2e | grep '.ts')"
 for TEST_IN in $TEST_INS
 do
   echo "RUNNING ${TEST_IN}"
-  export TEST_OUT="${TEST_IN::-3}.out"
+  export TEST_OUT="${TEST_IN::-3}.json"
   export EXPECTED_OUT="$(cat test/e2e/${TEST_OUT})"
   export ACTUAL_OUT="$(node build/cli.js --directory test/data --parse ${TEST_IN})"
   export DIFF="$(diff <(echo ${EXPECTED_OUT}) <(echo ${ACTUAL_OUT}))"
