90250079f98a75c039a2142f9db8c69e52d0ddce
Ari Chivukula <ari@chivuku.la>
2019-04-03 07:14:28 -0700

    Update column.ts

--- source/column.ts
+++ source/column.ts
@@ -1,4 +1,8 @@
 import {
+  WorkSheet,
+} from "xlsx";
+
+import {
   ICanExportAndExplain,
 } from "./common";
 import {
@@ -16,11 +20,18 @@ export interface IColumn extends IList {
 }
 
 export class Column extends List implements IColumn {
+  constructor(
+    private readonly ws: WorkSheet,
+    private readonly columnIdx: number,
+  ) {
+    super();
+  }
+
   protected async explore(
     appendToOutput: (key: string, value: ICanExportAndExplain) => Promise<void>,
   ): Promise<void> {
     for (const target of this.getTargets()) {
-      const cell = new Cell(target.parser);
+      const cell = new Cell(this.ws, target.index, this.rowIdx, target.parser);
       await appendToOutput(target.name + ":" + target.index, cell);
     }
   }
