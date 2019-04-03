86ad8d01a6a284380071b50498110f10fe1f554d
Ari Chivukula <ari@chivuku.la>
2019-04-03 05:35:21 -0700

    Update row.ts

--- source/row.ts
+++ source/row.ts
@@ -20,8 +20,7 @@ export class Row extends List implements IRow {
     appendToOutput: (key: string, value: ICanExportAndExplain) => Promise<void>,
   ): Promise<void> {
     for (const target of this.getTargets()) {
-      const cell = new Cell();
-      await target.parser(cell);
+      const cell = new Cell(target.parser);
       await appendToOutput(target.name + ":" + target.index, cell);
     }
   }
