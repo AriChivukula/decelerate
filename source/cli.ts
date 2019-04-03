0a5ae318e0a93f7b47a2472555bb3f12068efd67
Ari Chivukula <ari@chivuku.la>
2019-04-03 08:13:32 -0700

    Update cli.ts

--- source/cli.ts
+++ source/cli.ts
@@ -1,17 +1,38 @@
 import * as yargs from "yargs";
 
 import {
-} from "./index";
+  Directory,
+  DirectoryParser,
+} from "./directory";
 
 yargs
   .usage(
     "$0",
     true,
-    (y: yargs.Argv<any>): yargs.Argv<any> => y,
+    (y: yargs.Argv<any>): yargs.Argv<any> => y
+      .option(
+        "directory",
+        {
+          demandOption: true,
+          describe: "Root Data Path",
+        },
+      )
+      .option(
+        "parser",
+        {
+          demandOption: true,
+          describe: "Root Parser Path",
+        },
+      ),
     entryPoint,
   )
   .help()
   .argv;
 
 export async function entryPoint(argv: yargs.Arguments<any>): Promise<void> {
+  const directory = new Directory(argv.directory);
+  const parser: DirectoryParser = require(`${process.cwd()}/${argv.path}`);
+  await parser(directory);
+  const exported = await directory.export();
+  console.log(exported);
 }
