794a587450d862b849b774674bf99a547eae9954
Ari Chivukula <ari@chivuku.la>
2019-05-11 09:56:21 -0700

    Update parser.ts

--- source/parser.ts
+++ source/parser.ts
@@ -1,6 +1,9 @@
 /* EXPERIMENTAL */
 
 import * as yargs from "yargs";
+import {
+  promises,
+} from "fs";
 
 yargs
   .usage(
@@ -24,4 +27,18 @@ export function argParse(y: yargs.Argv<any>): yargs.Argv<any> {
 }
 
 export async function entryPoint(argv: yargs.Arguments<any>): Promise<void> {
+  const data: any = await promises.readFile(argv.data);
+  const clients: Client[] = [];
+  for (const clientName in data) {
+    clients.push(new Client(clientName, data[clientName]);
+  }
+}
+
+class Client {
+  constructor(
+    private readonly name: string,
+    private readonly data: any,
+  ) {
+    console.log(this.name);
+  }
 }
