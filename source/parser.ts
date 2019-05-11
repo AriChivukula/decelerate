33b0ef04afec569c825c17fb256ab71331e27600
Ari Chivukula <ari@chivuku.la>
2019-05-11 10:41:36 -0700

    Fix JSON

--- source/parser.ts
+++ source/parser.ts
@@ -4,6 +4,9 @@ import * as yargs from "yargs";
 import {
   promises,
 } from "fs";
+import {
+  parse,
+} from "relaxed-json";
 
 yargs
   .usage(
@@ -27,10 +30,11 @@ export function argParse(y: yargs.Argv<any>): yargs.Argv<any> {
 }
 
 export async function entryPoint(argv: yargs.Arguments<any>): Promise<void> {
-  const data: any = await promises.readFile(argv.data);
+  const data: any = await promises.readFile(argv.data, "ascii");
+  const json: any = JSON.parse(data);
   const clients: Client[] = [];
-  for (const clientName in data) {
-    clients.push(new Client(clientName, data[clientName]);
+  for (const clientName in json) {
+    clients.push(new Client(clientName, data[clientName]));
   }
 }
 
@@ -39,6 +43,6 @@ class Client {
     private readonly name: string,
     private readonly data: any,
   ) {
-    console.log(this.name);
+    console.log(name);
   }
 }
