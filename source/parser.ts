9baef7299fd6877ce15cc3072b347586e8e85150
Ari Chivukula <ari@chivuku.la>
2019-05-16 13:01:11 -0700

    done!

--- source/parser.ts
+++ source/parser.ts
@@ -67,11 +67,11 @@ function makeClient(client_name: string, data: any): Client {
     has_anxiety: checkForDiagnosis(data, "depression:7"),
     has_other: checkForDiagnosis(data, "other:8"),
     was_raped: checkForHarmType(data, "RA"),
-    was_raped_as_a_child: false, // TODO
+    was_raped_as_a_child: checkForHarmType(data, "RA", "CH"),
     was_sexually_abused: checkForHarmType(data, "SA"),
-    was_sexually_abused_as_a_child: false, // TODO
+    was_sexually_abused_as_a_child: checkForHarmType(data, "SA", "CH"),
     was_physically_harmed: checkForHarmType(data, "PH"),
-    was_physically_harmed_as_a_child: false, // TODO
+    was_physically_harmed_as_a_child: checkForHarmType(data, "PH", "CH"),
     was_harmed_by_state_actor: checkForHarmActor(data, "SA"),
     was_harmed_by_police: checkForHarmActor(data, "PO"),
   };
@@ -122,7 +122,7 @@ function checkForHarmActor(data: any, actor: string): boolean {
   return false;
 }
 
-function checkForHarmType(data: any, harm_type: string): boolean {
+function checkForHarmType(data: any, harm_type: string, at_age: string | null = null): boolean {
   for (const sheetName in data) {
     if (!sheetName.includes("Harm Details")) {
       continue;
@@ -130,7 +130,13 @@ function checkForHarmType(data: any, harm_type: string): boolean {
     for (const rowName in data[sheetName]) {
       const types: string = getSubData(data, sheetName, rowName, "type:1");
       if (types.includes(harm_type)) {
-        return true;
+        if (at_age === null) {
+          return true;
+        }
+        const age: string = getSubData(data, sheetName, rowName, "age:3");
+        if (types.includes(at_age)) {
+          return true;
+        }
       }
     }
   }
